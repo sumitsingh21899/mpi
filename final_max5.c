97c4b12d10bf8086dff2291b653a6b9ffcaa9a94
skippersumit <49806908+skippersumit@users.noreply.github.com>
2019-07-01 05:56:39 +0530

    Update final_max5.c

--- final_max5.c
+++ final_max5.c
@@ -20,7 +20,7 @@ int main (int argc, char *argv[])
     numtasks=tasks-1;
     
     MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
-//    printf ("MPI task %d has started...\n", taskid);
+   printf ("MPI task %d has started...\n", taskid);
    
     tag2 = 1;
     tag1 = 2;
@@ -31,25 +31,25 @@ int main (int argc, char *argv[])
             data[i] =  i;
             max=(max>data[i])?max:data[i];
             }
-  //  printf("Initialized array max = %e\n",max);
+   printf("Initialized array max = %e\n",max);
     gettimeofday(&in,NULL);
         offset=0;
     for (dest=1; dest<tasks; dest++) {
         chunksize=(dest*ARRAYSIZE)/numtasks - ((dest-1)*(ARRAYSIZE))/numtasks;
             MPI_Send(&offset, 1, MPI_INT, dest, tag1, MPI_COMM_WORLD);
             MPI_Send(&data[offset], chunksize, MPI_FLOAT, dest, tag2, MPI_COMM_WORLD);
-    //        printf("Sent %d elements to task %d offset= %d\n",chunksize,dest,offset);
+           printf("Sent %d elements to task %d offset= %d\n",chunksize,dest,offset);
             offset = offset + chunksize;
     }
  mymax=0;
     MPI_Reduce(&mymax, &max, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD);
     
-   // printf("Sample results: \n");
+    printf("Sample results: \n");
     offset = 0;
-   // printf("*** Final max= %e ***\n",max);
+    printf("*** Final max= %e ***\n",max);
     gettimeofday(&f,NULL);
-   // float elapsed = (f.tv_sec*1000000 + f.tv_usec) -
-     //           (in.tv_sec*1000000 + in.tv_usec);
+    //float elapsed = (f.tv_sec*1000000 + f.tv_usec) -
+      //          (in.tv_sec*1000000 + in.tv_usec);
 float elapsed = (f.tv_sec) -
                 (in.tv_sec);
      
@@ -60,11 +60,9 @@ printf("Elapsed time  = %f seconds \n",elapsed);
           source = 0;
         chunksize=(taskid*ARRAYSIZE)/numtasks - ((taskid-1)*(ARRAYSIZE))/numtasks;
           MPI_Recv(&offset, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
-          MPI_Recv(&data[offset], chunksize, MPI_FLOAT, source, tag2, 
-            MPI_COMM_WORLD, &status);
+          MPI_Recv(&data[offset], chunksize, MPI_FLOAT, source, tag2, MPI_COMM_WORLD, &status);
 
           mymax = update(offset, chunksize, taskid);
-        max=(max>mymax)?max:mymax;
           MPI_Reduce(&mymax, &max, 1, MPI_FLOAT, MPI_MAX, 0, MPI_COMM_WORLD);
   } 
     MPI_Finalize();
@@ -77,6 +75,6 @@ float update(int myoffset, int chunk, int myid) {
             data[i] =  i ;
             mymax=(mymax>data[i])?mymax:data[i];
     }
-  //  printf("Task %d mymax = %e\n",myid,mymax);
+    printf("Task %d mymax = %e\n",myid,mymax);
     return(mymax);
 }
